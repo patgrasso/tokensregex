9227317491b2b346b7dead5b564851bffcba1222
patgrasso <pgrasso@stevens.edu>
2018-01-03 03:42:17 -0500

    Initial commit -- parser + NFA
    - Parser parses TokensRegex and turns it into postfix for easier NFA
      creation.
    - nfa.create() creates a State which is the start state for the
      simulator.
    
    test/ contains tests

---
+++ lib/ts2postfix.y
@@ -0,0 +1,46 @@
+%token REGEXP LBRACE RBRACE LPAREN RPAREN LBRAK RBRAK COLON WORD OR AND
+%token MAYBE KLEENE PLUS SP SEMICOLON
+
+%%
+regex     : exp                     { return $1; } ;
+
+exp       : exp sp maybe            { $$=[].concat($1, $3, [["."]]); }
+          | maybe                   { $$=$1; }
+          ;
+
+maybe     : quant MAYBE             { $$=[].concat($1, [["?"]]); }
+          | quant                   { $$=$1; }
+          ;
+
+quant     : atom KLEENE             { $$=[].concat($1, [["*"]]); }
+          | atom PLUS               { $$=[].concat($1, [["+"]]); }
+          | atom                    { $$=$1; }
+          ;
+                                        /*maybe illegal*/
+oneof     : oneof atom              { $$=[].concat($1, $2, [["|"]]); }
+          | oneof binop atom        { $$=[].concat($1, $3, $2); }
+          | atom                    { $$=$1; }
+          ;
+
+binop     : sp OR sp                { $$=[["|"]]; }
+          | sp AND sp               { $$=[["&"]]; }
+          ;
+
+atom      : LPAREN sp exp sp RPAREN { $$=$3; }
+          | LBRAK sp oneof sp RBRAK { $$=$3; }
+          | LBRACE sp atr sp RBRACE { $$=$3; }
+          | REGEXP                  { $$=[[ "REGEXP" , new RegExp($1) ]]; }
+          | WORD                    { $$=[[ "WORD"   , $1 ]]; }
+          ;
+
+atr       : atr SEMICOLON prop      { $$=[].concat($1, $3, [["&"]]); }
+          | prop                    { $$=$1; }
+          ;
+
+prop      : WORD sp COLON sp REGEXP { $$=[[ "ATTR", $1, new RegExp($5) ]]; }
+          | WORD sp COLON sp WORD   { $$=[[ "ATTR", $1, $5 ]]; }
+          ;
+
+
+sp        : SP | ;
+%%
---
+++ test/ts2postfix.y.test.js
@@ -0,0 +1,222 @@
+const expect  = require('chai').expect;
+const parser  = require('../lib/ts2postfix.js');
+const parse   = parser.parse;
+
+describe('ts2postfix.y', () => {
+
+  describe('lex(ATTR) \'{ <attr>:<value> }\'', () => {
+
+    it('allows spaces around braces', () => {
+      expect(parse('{ attr:my }')[0][0]).to.equal('ATTR');
+      expect(parse('{ \tattr:my\t\n }')[0][0]).to.equal('ATTR');
+    });
+
+    it('allows spaces around colon', () => {
+      expect(parse('{attr : my}')).to.eql([[ 'ATTR', 'attr', 'my' ]]);
+      expect(parse('{attr \t:\t\n   my}')).to.eql([[ 'ATTR', 'attr', 'my' ]]);
+    });
+
+    it('stores <attr> in the 1st index', () => {
+      expect(parse('{attr:my}')[0][1]).to.equal('attr');
+      expect(parse('{word:my}')[0][1]).to.equal('word');
+    });
+
+    it('stores <value> in the 2nd index', () => {
+      expect(parse('{attr:my}')[0][2]).to.equal('my');
+      expect(parse('{word:my}')[0][2]).to.equal('my');
+      expect(parse('{word:foobar}')[0][2]).to.equal('foobar');
+    });
+
+    it('returns a RegExp instance if <value> is a REGEXP', () => {
+      expect(parse('{attr:/my/}')[0][2]).to.eql(/my/);
+      expect(parse('{attr:/my/}')[0][2]).to.not.eql('my'); // sanity check
+    });
+
+  });
+
+  describe('lex(WORD)', () => {
+
+    it('recognizes a word consisting of a-zA-Z', () => {
+      let lowerAlpha  = 'abcdefghijklm nopqrstuvwxyz'
+        , lowerAlpha0 = 'abcdefghijklm'
+        , lowerAlpha1 = 'nopqrstuvwxyz'
+        , upperAlpha  = 'ABCDEFGHIJKLM NOPQRSTUVWXYZ'
+        , upperAlpha0 = 'ABCDEFGHIJKLM'
+        , upperAlpha1 = 'NOPQRSTUVWXYZ';
+
+      expect(parse(lowerAlpha)).to.eql([
+        [ 'WORD', lowerAlpha0 ],
+        [ 'WORD', lowerAlpha1 ],
+        [ '.' ]
+      ]);
+      expect(parse(upperAlpha)).to.eql([
+        [ 'WORD', upperAlpha0 ],
+        [ 'WORD', upperAlpha1 ],
+        [ '.' ]
+      ]);
+    });
+
+    it('recognizes a word consisting of 0-9', () => {
+      expect(parse('0123456789')).to.eql([[ 'WORD', '0123456789' ]]);
+    });
+
+    // TODO: i mean, we *should* test every character except those excluded
+    //       by the regex (see lex file)
+
+  });
+
+  describe('\'.\' concat', () => {
+
+    it('a => a', () => {
+      expect(parse('a')).to.eql([
+        [ 'WORD', 'a' ]
+      ]);
+    });
+
+    it('a b => a b .', () => {
+      expect(parse('a b')).to.eql([
+        [ 'WORD', 'a' ],
+        [ 'WORD', 'b' ],
+        [ '.' ]
+      ]);
+    });
+
+    it('a /b/ => a /b/ .', () => {
+      expect(parse('a /b/')).to.eql([
+        [ 'WORD', 'a' ],
+        [ 'REGEXP', /b/ ],
+        [ '.' ]
+      ]);
+      expect(parse('a { word:b }')).to.eql([
+        [ 'WORD', 'a' ],
+        [ 'ATTR', 'word', 'b' ],
+        [ '.' ]
+      ]);
+    });
+
+    it('a b (c d) => a b . c d . .', () => {
+      expect(parse('a b (c d)')).to.eql([
+        [ 'WORD', 'a' ],
+        [ 'WORD', 'b' ],
+        [ '.' ],
+        [ 'WORD', 'c' ],
+        [ 'WORD', 'd' ],
+        [ '.' ],
+        [ '.' ]
+      ]);
+    });
+
+    it('a { word:hello } b => a { word:hello } . b .', () => {
+      expect(parse('a { word:hello } b')).to.eql([
+        [ 'WORD', 'a' ],
+        [ 'ATTR', 'word', 'hello' ],
+        [ '.' ],
+        [ 'WORD', 'b' ],
+        [ '.' ]
+      ]);
+    });
+
+    it('a ([ /[abc]def[cba]/ & /c/ | d ]) => a /[abc]def[cba]/ /c/ & d | .', () => {
+      expect(parse('a ([ /[abc]def[cba]/ & /c/ | d ])')).to.eql([
+        [ 'WORD', 'a' ],
+        [ 'REGEXP', /[abc]def[cba]/ ],
+        [ 'REGEXP', /c/ ],
+        [ '&' ],
+        [ 'WORD', 'd' ],
+        [ '|' ],
+        [ '.' ]
+      ]);
+    });
+
+  });
+
+  [ ['|', 'or'], ['&', 'and'] ].forEach(([ o, desc ]) => {
+    describe(`'${o}' ${desc}`, () => {
+
+      it(`[ a ${o} b ] => a b ${o}`, () => {
+        expect(parse(`[ a ${o} b ]`)).to.eql([
+          [ 'WORD', 'a' ],
+          [ 'WORD', 'b' ],
+          [ o ]
+        ]);
+      });
+
+      it(`[ a ${o} b ${o} c ] => a b ${o} c ${o}`, () => {
+        expect(parse(`[ a ${o} b ${o} c ]`)).to.eql([
+          [ 'WORD', 'a' ],
+          [ 'WORD', 'b' ],
+          [ o ],
+          [ 'WORD', 'c' ],
+          [ o ]
+        ]);
+      });
+
+      it('throws if not inside [ ... ]', () => {
+        expect(() => parse( `a ${o} b` )).to.throw();
+        expect(() => parse(`(a ${o} b)`)).to.throw();
+        expect(() => parse(`[a ${o} b]`)).not.to.throw();
+        expect(() => parse(`([a ${o} b])`)).not.to.throw();
+        expect(() => parse(`([( a ${o} b )])`)).to.throw();
+        expect(() => parse(`((([a ${o} b])))`)).not.to.throw();
+      });
+
+    });
+  });
+
+  describe('\'?\' maybe', () => {
+
+    it('a?b => a ? b .', () => {
+      expect(parse('a?b')).to.eql([
+        [ 'WORD', 'a' ],
+        [ '?' ],
+        [ 'WORD', 'b' ],
+        [ '.' ]
+      ]);
+    });
+
+    it('[a | b]? c => a b | ? c .', () => {
+      expect(parse('[a | b]? c')).to.eql([
+        [ 'WORD', 'a' ],
+        [ 'WORD', 'b' ],
+        [ '|' ],
+        [ '?' ],
+        [ 'WORD', 'c' ],
+        [ '.' ]
+      ]);
+    });
+
+    it('throws if after a space ("a ?" rather than "a?")', () => {
+      expect(() => parse('a ?')).to.throw();
+      expect(() => parse('a?')).not.to.throw();
+    });
+
+  });
+
+  let other = { '+': '*', '*': '+' };
+
+  [ ['+', 'plus'], ['*', 'kleene star'] ].forEach(([ o, desc ]) => {
+    describe(`'${o}' ${desc}`, () => {
+
+      it('a+b => a + b .', () => {
+        expect(parse(`a${o}b`)).to.eql([
+          [ 'WORD', 'a' ],
+          [ o ],
+          [ 'WORD', 'b' ],
+          [ '.' ]
+        ]);
+      });
+
+      it(`throws if after a ${other[o]} (e.g. "a${other[o]}${o}")`, () => {
+        expect(() => parse(`a${other[o]}${o}`)).to.throw();
+        expect(() => parse(`a${o}`)).not.to.throw();
+      });
+
+      it(`throws if after a space ("a ${o}" rather than "a${o}")`, () => {
+        expect(() => parse(`a ${o}`)).to.throw();
+        expect(() => parse(`a${o}`)).not.to.throw();
+      });
+
+    });
+  });
+
+});
